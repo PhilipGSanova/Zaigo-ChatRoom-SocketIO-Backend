e28f0bfd1442460f3c821f47b0fc5d09a212c46e
PhilipGSanova <sanov2004@gmail.com>
2025-11-27 12:47:30 +0530

    Update2

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: 'https://zaigo-chatroom-socketio-frontend.onrender.com',
   credentials: true
 }));
 
