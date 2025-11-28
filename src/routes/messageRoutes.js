0ee4c8ee45143372dd7e85c43caadd1668f48302
PhilipGSanova <sanov2004@gmail.com>
2025-11-28 11:09:49 +0530

    28-11-2

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Message = require("../models/Message");
-const auth = require("../middleware/authMiddleware"); // must extract req.user
+const auth = require("../middleware/auth");
 
 router.get("/:roomId", auth, async (req, res) => {
   try {
