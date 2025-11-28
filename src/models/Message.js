b5f2daaa2fdc88c1c46a16ffc775cb7c238840a9
PhilipGSanova <sanov2004@gmail.com>
2025-11-28 10:51:09 +0530

    Modified Message Schema

--- src/models/Message.js
+++ src/models/Message.js
@@ -3,8 +3,8 @@ const mongoose =  require('mongoose');
 const messageSchema = new mongoose.Schema({
     room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
     sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    content: { type: String },
-    attachements: [{ url: String, filename: String, mime: String}],
+    text: { type: String },
+    attachments: [{ url: String, filename: String, mime: String}],
     reactions: [{ user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, emoji: String }],
     readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     createdAt: { type: Date, default: Date.now },
