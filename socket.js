da968ad5f95931763b30798e1584d1b1829be51f
PhilipGSanova <sanov2004@gmail.com>
2025-12-11 12:25:25 +0530

    11-12-2

--- socket.js
+++ socket.js
@@ -1,7 +1,6 @@
 const { Server } = require("socket.io");
 const jwt = require("jsonwebtoken");
 const Message = require("./src/models/Message");
-const Room = require("./src/models/Room");
 
 const jwtSecret = process.env.JWT_SECRET || "changeme";
 let io;
@@ -15,7 +14,7 @@ function initSocket(server) {
     },
   });
 
-  // Auth middleware
+  // Auth
   io.use((socket, next) => {
     const token = socket.handshake.auth?.token;
     if (!token) return next(new Error("Authentication error"));
@@ -31,79 +30,54 @@ function initSocket(server) {
   io.on("connection", (socket) => {
     console.log("Connected:", socket.user.username);
 
-    // Join personal room
     socket.join(socket.user.id);
 
-    // Join chatroom
+    // Join Room
     socket.on("join_room", ({ roomId }) => {
       socket.join(roomId);
-      socket.to(roomId).emit("user_joined", {
-        userId: socket.user.id,
-        username: socket.user.username,
-      });
     });
 
-    // Leave chatroom
-    socket.on("leave_room", ({ roomId }) => {
-      socket.leave(roomId);
-      socket.to(roomId).emit("user_left", {
-        userId: socket.user.id,
-        username: socket.user.username,
-      });
-    });
-
-    // TEXT MESSAGE
+    // Text Message
     socket.on("send_message", async ({ roomId, text }) => {
       if (!roomId || !text) return;
 
-      try {
-        const message = await Message.create({
-          room: roomId,
-          sender: socket.user.id,
-          text,
-        });
+      const message = await Message.create({
+        room: roomId,
+        sender: socket.user.id,
+        text,
+      });
 
-        io.to(roomId).emit("new_message", {
-          _id: message._id,
-          text,
-          roomId,
-          sender: socket.user,
-          createdAt: message.createdAt,
-        });
-      } catch (err) {
-        console.error(err);
-      }
+      io.to(roomId).emit("new_message", {
+        ...message.toObject(),
+        sender: socket.user,
+      });
     });
 
-    // VOICE MESSAGE
+    // Voice
     socket.on("send_voice_message", async ({ roomId, audio }) => {
-      try {
-        const message = await Message.create({
-          room: roomId,
-          audio,
-          sender: socket.user.id,
-          createdAt: new Date(),
-        });
+      const message = await Message.create({
+        room: roomId,
+        sender: socket.user.id,
+        audio,
+      });
 
-        io.to(roomId).emit("new_voice_message", {
-          _id: message._id,
-          audio,
-          roomId,
-          sender: socket.user,
-          createdAt: message.createdAt,
-        });
-      } catch (err) {
-        console.error("Voice msg error:", err);
-      }
+      io.to(roomId).emit("new_voice_message", {
+        ...message.toObject(),
+        sender: socket.user,
+      });
     });
 
-    // Typing
-    socket.on("typing", ({ roomId, isTyping }) => {
-      socket.to(roomId).emit("typing_status", {
-        roomId,
-        userId: socket.user.id,
-        username: socket.user.username,
-        isTyping,
+    // Image (NEW)
+    socket.on("send_image_message", async ({ roomId, fileData }) => {
+      const message = await Message.create({
+        room: roomId,
+        sender: socket.user.id,
+        attachments: [fileData],
+      });
+
+      io.to(roomId).emit("new_image_message", {
+        ...message.toObject(),
+        sender: socket.user,
       });
     });
 
@@ -113,7 +87,4 @@ function initSocket(server) {
   });
 }
 
-module.exports = {
-  initSocket,
-  getIO: () => io, // allows routes to emit events
-};
+module.exports = { initSocket, getIO: () => io };
