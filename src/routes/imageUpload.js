c9d6374c8d854a60181dc70d975fde8f23eff296
PhilipGSanova <sanov2004@gmail.com>
2025-12-11 13:59:36 +0530

    11-12-6

--- src/routes/imageUpload.js
+++ src/routes/imageUpload.js
@@ -11,6 +11,8 @@ const upload = multer(); // memory storage
 router.post("/image", upload.single("attachment"), async (req, res) => {
   try {
     const io = getIO();
+    console.log("req.body:", req.body);
+    console.log0("req.file:", req.file);
     const { roomId, senderId, text } = req.body;
     const file = req.file;
 
